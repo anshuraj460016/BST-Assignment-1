11abaac7d671ba11833634d4b493b768e7e3b6e4
unknown <anshu2.r4616@gmail.com>
2023-11-21 20:52:05 +0530

    BST Assignment 1

---
+++ Q1.cpp
@@ -0,0 +1,45 @@
+//Find the nodes with minimum and maximum value in a Binary Search Tree.
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val){
+     this->val=val;
+     this->left=NULL;
+     this->right=NULL;
+    }
+
+};
+int max(Node*root){
+    if(!root) return -1;
+    if(!root->left and !root->right) return root->val;
+    max(root->right);
+}
+int min(Node*root){
+    if(!root) return -1;
+    if(!root->left and !root->right) return root->val;
+    min(root->left);
+}
+
+int main(){
+Node*a=new Node(1);
+Node*b=new Node(2);
+Node*c=new Node(3);
+Node*d=new Node(4);
+Node*e=new Node(5);
+Node*f=new Node(6);
+
+a->left=b;
+a->right=c;
+b->left=d;
+b->right=e;
+c->left=f;
+
+ cout<<min(a);
+ cout<<max(a);
+ return 0;
+
+}
